a2bb27c934031f6157542489c05a27dbbd4ad42e
Nick Neely <neelynickolas@gmail.com>
2023-11-15 19:02:38 -0600

    Fixed virtual environment

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Hades\OneDrive - Indian Hills Community College\Python\Recipe Sharing\env
+set VIRTUAL_ENV=C:\Users\neely\OneDrive - Indian Hills Community College\Python\Recipe Sharing\env
 
 if not defined PROMPT set PROMPT=$P$G
 
